860e19571b522a08cd561449a94fd78956b80252
Zaza Gordeziani <zg.gordeziani@gmail.com>
2026-04-15 14:39:34 +0400

    change styling of header, text size and padding for sing in and sign up buttons were changed, in addition text color and size of the text of browese courses was changed

--- src/components/base/header/header.tsx
+++ src/components/base/header/header.tsx
@@ -7,7 +7,10 @@ type Props = {
     onSignUpClick?: () => void
 }
 
-export default function Header({ onLoginClick, onSignUpClick }: Readonly<Props>) {
+export default function Header({
+    onLoginClick,
+    onSignUpClick,
+}: Readonly<Props>) {
     return (
         <header className="mx-auto flex h-27 w-full max-w-480 items-center justify-center border-b border-[#D1D1D1] bg-[#f5f5f5]">
             <div className="flex h-15 w-full max-w-391.5 items-center justify-between">
@@ -22,7 +25,7 @@ export default function Header({ onLoginClick, onSignUpClick }: Readonly<Props>)
                 <div className="flex items-center gap-9">
                     <button
                         type="button"
-                        className="font-inter flex items-center gap-2 text-[16px] leading-6 font-normal text-black"
+                        className="font-inter flex items-center gap-2 text-xl leading-6 font-normal text-[#525252]"
                     >
                         <img
                             src={browseCoursesIcon}
@@ -33,17 +36,17 @@ export default function Header({ onLoginClick, onSignUpClick }: Readonly<Props>)
                         <span>Browse Courses</span>
                     </button>
 
-                    <div className="font-inter flex items-center gap-2 text-[16px] leading-6 font-normal">
+                    <div className="font-inter flex items-center gap-3 text-xl leading-6 font-normal">
                         <button
                             type="button"
-                            className="h-15 w-28.5 rounded-lg border-2 border-[#958FEF] bg-white px-4 py-3 text-[#958FEF]"
+                            className="rounded-lg border-2 border-[#958FEF] bg-white px-5 py-4 text-[#958FEF]"
                             onClick={onLoginClick}
                         >
                             Log In
                         </button>
                         <button
                             type="button"
-                            className="h-15 w-28.5 rounded-lg border-2 border-[#4F46E5] bg-[#4F46E5] px-4 py-3 text-white"
+                            className="rounded-lg border-2 border-[#4F46E5] bg-[#4F46E5] px-5 py-4 text-white"
                             onClick={onSignUpClick}
                         >
                             Sign Up
